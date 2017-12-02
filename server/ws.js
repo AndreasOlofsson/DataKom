bd94247e8d6dc8cb899f9c1db2ea54b16bb554f1
MelkerForssell <melkerforssell@gmail.com>
2017-12-02 10:50:27 +0100

    La till addbooking i WS, flyttade ut validDate ur getAvailable då den lär användas på flera ställen.

--- server/ws.js
+++ server/ws.js
@@ -8,84 +8,119 @@ module.exports = (server, db) => {
     }
 
     async function getAvailable(ws, msg) {
-        if (msg["date"]) {
-            let date = msg["date"];
-            
-            if (typeof date !== "string") {
-                throw "Bad Request (\"date\" is not a string)";
+        if (msg['date']) {
+            let date = msg['date'];
+
+            if (typeof date !== 'string') {
+                throw 'Bad Request ("date" is not a string)';
             }
-            
-            date = date.split("-");
-            
+
+            date = date.split('-');
+
             if (date.length !== 3) {
-                throw "Bad Request (\"date\" must be in the format \"YYYY-MM-DD\")";
+                throw 'Bad Request ("date" must be in the format "YYYY-MM-DD")';
             }
 
             let available;
-            
+
             try {
-                available = await db.dayAvailable(new Date(Date.UTC(date[0], date[1] - 1, date[2])));
+                available = await db.dayAvailable(
+                    new Date(Date.UTC(date[0], date[1] - 1, date[2]))
+                );
             } catch (e) {
-                throw "Server Error (DB access failed)"
+                throw 'Server Error (DB access failed)';
             }
-            
+
             return {
-                "available": available
+                available: available
             };
         } else {
-            throw "bad request (\"date\" missing)";
+            throw 'bad request ("date" missing)';
         }
+    }
+    /*
+expectedArray = {
+    "name":"John",
+    "email":'melkerforssell@gmail.com',
+    "date": date,
+    "number": numPeople,
+    "text" : text,
+}
+*/
+
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
     }
 
     async function addBooking(ws, msg) {
-        if (msg["booking"]) {
-            return {
-                "msg": "test"
-            };
+        if (msg['booking']) {
+            let booking = msg['booking'];
+            if (booking.name.length < 5) {
+                throw 'Bookings require fullname';
+            }
+            if (booking.name.length < 5) {
+                throw 'Bookings require fullname';
+            }
+            if (!validateEmail(booking.email)) {
+                throw 'Not valid emailadress';
+            }
+
+            try {
+                booking = await db.addBooking(
+                    booking.name,
+                    booking.email,
+                    booking.date,
+                    booking.number,
+                    booking.text
+                );
+            } catch (e) {
+                throw 'Server Error (DB access failed)';
+            }
         } else {
-            throw "bad request (\"booking\" missing)";
+            throw 'bad request ("booking" missing)';
         }
     }
-    
+
     wss.on('connection', (ws, req) => {
-        ws.on('message', (msg) => {
+        ws.on('message', msg => {
             (async function() {
                 let err;
-                
+
                 try {
                     msg = JSON.parse(msg);
 
                     const func = {
                         getAvailable: getAvailable,
                         addBooking: addBooking
-                    }[msg["request"]];
-                    
+                    }[msg['request']];
+
                     if (func) {
                         try {
                             let result = await func(ws, msg);
 
-                            result["result"] = "ok";
-                            result["id"] = msg["id"];
+                            result['result'] = 'ok';
+                            result['id'] = msg['id'];
 
                             sendJSON(ws, result);
                         } catch (e) {
                             err = e;
                         }
                     } else {
-                        err = "Bad Request (function not found)";
+                        err = 'Bad Request (function not found)';
                     }
                 } catch (_) {
-                    err = "Invalid JSON";
+                    err = 'Invalid JSON';
                 }
-                
+
                 if (err) {
                     try {
                         sendJSON(ws, {
                             result: err,
-                            id: msg["id"]
+                            id: msg['id']
                         });
                     } catch (e) {
-                        console.error("Failed to send error message: ");
+                        console.error('Failed to send error message: ');
                         console.error(err);
                     }
                 }
