11f866407eacf95ba506e9870610b6689346a7ba
MelkerForssell <melkerforssell@gmail.com>
2017-12-06 11:19:30 +0100

    fel med brackets

--- server/ws.js
+++ server/ws.js
@@ -10,25 +10,25 @@ module.exports = (server, db) => {
     async function getAvailable(ws, msg) {
         if (msg["date"]) {
             let date = msg["date"];
-            
+
             if (typeof date !== "string") {
                 throw "Bad Request (\"date\" is not a string)";
             }
-            
+
             date = date.split("-");
-            
+
             if (date.length !== 3) {
                 throw "Bad Request (\"date\" must be in the format \"YYYY-MM-DD\")";
             }
 
             let available;
-            
+
             try {
                 available = await db.dayAvailable(new Date(Date.UTC(date[0], date[1] - 1, date[2])));
             } catch (e) {
                 throw "Server Error (DB access failed)"
             }
-            
+
             return {
                 "available": available
             };
@@ -36,7 +36,7 @@ module.exports = (server, db) => {
             throw "bad request (\"date\" missing)";
         }
     }
-<<<<<<< HEAD
+
 
     function validateEmail(email) {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -78,19 +78,20 @@ module.exports = (server, db) => {
                 throw 'Server Error (DB access failed)';
             }
             return booking;
-=======
+}
+}
 
     async function addBooking(ws, msg) {
         if (msg["booking"]) {
             return {
                 "msg": "test"
             };
->>>>>>> cb514ab69e64e656db42c21765c49f50b24d56e5
+
         } else {
             throw "bad request (\"booking\" missing)";
         }
     }
-<<<<<<< HEAD
+
 
     async function markDayAsFull(ws, msg) {
         if (msg['markDayAsFull']) {
@@ -108,13 +109,13 @@ module.exports = (server, db) => {
     }
 
 =======
-    
+
 >>>>>>> cb514ab69e64e656db42c21765c49f50b24d56e5
     wss.on('connection', (ws, req) => {
         ws.on('message', (msg) => {
             (async function() {
                 let err;
-                
+
                 try {
                     msg = JSON.parse(msg);
 
@@ -122,7 +123,7 @@ module.exports = (server, db) => {
                         getAvailable: getAvailable,
                         addBooking: addBooking
                     }[msg["request"]];
-                    
+
                     if (func) {
                         try {
                             let result = await func(ws, msg);
@@ -140,7 +141,7 @@ module.exports = (server, db) => {
                 } catch (_) {
                     err = "Invalid JSON";
                 }
-                
+
                 if (err) {
                     try {
                         sendJSON(ws, {
