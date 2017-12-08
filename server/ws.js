5beb71997b7f32fe536ad226a261c95311f35ad0
MelkerForssell <melkerforssell@gmail.com>
2017-12-08 11:07:10 +0100

    mer dumfel

--- server/ws.js
+++ server/ws.js
@@ -8,98 +8,92 @@ module.exports = (server, db) => {
     }
 
     async function getAvailable(ws, msg) {
-            let date = msg["date"];
+        let date = msg["date"];
 
-            if (typeof date !== "string") {
-                throw 'Bad Request ("date" is not a string)';
-            }
-
-            date = date.split("-");
-
-            if (date.length !== 3) {
-                throw 'Bad Request ("date" must be in the format "YYYY-MM-DD")';
-            }
-
-            let available;
-
-            try {
-                available = await db.dayAvailable(
-                    new Date(Date.UTC(date[0], date[1] - 1, date[2]))
-                );
-            } catch (e) {
-                throw "Server Error (DB access failed)";
-            }
-
-            return {
-                available: available
-            };
+        if (typeof date !== "string") {
+            throw 'Bad Request ("date" is not a string)';
         }
 
+        date = date.split("-");
+
+        if (date.length !== 3) {
+            throw 'Bad Request ("date" must be in the format "YYYY-MM-DD")';
+        }
+
+        let available;
+
+        try {
+            available = await db.dayAvailable(new Date(Date.UTC(date[0], date[1] - 1, date[2])));
+        } catch (e) {
+            throw "Server Error (DB access failed)";
+        }
+
+        return {
+            available: available
+        };
+    }
 
     async function getBookingsDate(ws, msg) {
-            var date = msg["getBookingsDate"];
+        var date = msg["getBookingsDate"];
 
-            date = date.split("-");
-            let bookings;
-            try {
-                bookings = await db.getBookings(
-                    new Date(Date.UTC(date[0], date[1] - 1, date[2])),
-                    new Date(Date.UTC(date[0], date[1] - 1, date[2]+1))
-                );
-            } catch (e) {
-                throw "Server Error (DB access failed)";
-            }
-            return {
-                bookings: bookings
-            };
+        date = date.split("-");
+        let bookings;
+        try {
+            bookings = await db.getBookings(
+                new Date(Date.UTC(date[0], date[1] - 1, date[2])),
+                new Date(Date.UTC(date[0], date[1] - 1, date[2] + 1))
+            );
+        } catch (e) {
+            throw "Server Error (DB access failed)";
         }
-
+        return {
+            bookings: bookings
+        };
+    }
 
     async function removeBooking(ws, msg) {
-            var id = msg["removeBooking"];
-            try {
-                await db.removeBooking(id);
-            } catch (e) {
-                throw "Server Error (DB access failed)";
-            }
-            return true;
+        var id = msg["removeBooking"];
+        try {
+            await db.removeBooking(id);
+        } catch (e) {
+            throw "Server Error (DB access failed)";
+        }
+        return true;
     }
 
     async function confirmBooking(ws, msg) {
-            var id = msg["confirmBooking"];
-            try {
-                await db.changeBookingStatus(id, "Confirmed");
-            } catch (e) {
-                throw "Error on confirm booking";
-            }
-            return true;
-      }
-
-
-    async function unConfirmBooking(ws, msg) {
-            var id = msg["unConfirmBooking"];
-            try {
-                await db.changeBookingStatus(id, "Pending");
-            } catch (e) {
-                throw "Error on unConfirmBooking";
-            }
-            return true;
-        }
-
-    async function getDaysWithBooking(ws, msg) {
-      var date = msg["getDaysWithBooking"];
-      date = date.split("-");
-        var available = [];
+        var id = msg["confirmBooking"];
         try {
-          for (i = 0; i < 30; i++) {
-              available[i] = await db.dayAvailable(new Date Date.UTC(date[0],date[1]-1,i )
+            await db.changeBookingStatus(id, "Confirmed");
+        } catch (e) {
+            throw "Error on confirm booking";
         }
-      }catch (e) {
-          throw "Server Error (DB access failed)";
-      }
-      return available;
+        return true;
     }
 
+    async function unConfirmBooking(ws, msg) {
+        var id = msg["unConfirmBooking"];
+        try {
+            await db.changeBookingStatus(id, "Pending");
+        } catch (e) {
+            throw "Error on unConfirmBooking";
+        }
+        return true;
+    }
+
+    async function getDaysWithBooking(ws, msg) {
+        var date = msg["getDaysWithBooking"];
+        date = date.split("-");
+        var available = [];
+        try {
+            for (i = 0; i < 30; i++) {
+                available[i] = await db.dayAvailable(new Date(Date.UTC(date[0], date[1] - 1, i)));
+            }
+        } catch (e) {
+            throw "Server Error (DB access failed)";
+        }
+        return available;
+    }
 
     function validateEmail(email) {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -120,37 +114,37 @@ module.exports = (server, db) => {
     }
 
     async function addBooking(ws, msg) {
-            let booking = msg["booking"];
-            if (booking.name.length < 1) {
-                throw "Name is required";
-            }
-            if (!validateEmail(booking.email)) {
-                throw "not valid emailadress!";
-            }
+        let booking = msg["booking"];
+        if (booking.name.length < 1) {
+            throw "Name is required";
+        }
+        if (!validateEmail(booking.email)) {
+            throw "not valid emailadress!";
+        }
 
+        try {
+            booking = await db.addBooking(
+                booking.name,
+                booking.email,
+                booking.date, // blir -1 på servern
+                booking.amountGuests,
+                booking.text
+            );
+        } catch (e) {
+            throw "Server Error (DB access failed)";
+        }
+        return booking;
+    }
+
+    async function markDayAsFull(ws, msg) {
+        let date = msg["markDayAsFull"];
+        if (validDate(date)) {
             try {
-                booking = await db.addBooking(
-                    booking.name,
-                    booking.email,
-                    booking.date, // blir -1 på servern
-                    booking.amountGuests,
-                    booking.text
-                );
+                response = await db.markDayAsFull(date);
             } catch (e) {
                 throw "Server Error (DB access failed)";
             }
-            return booking;
         }
-
-async function markDayAsFull(ws, msg) {
-            let date = msg["markDayAsFull"];
-            if (validDate(date)) {
-                try {
-                    response = await db.markDayAsFull(date);
-                } catch (e) {
-                    throw "Server Error (DB access failed)";
-                }
-            }
     }
 
     wss.on("connection", (ws, req) => {
@@ -167,7 +161,7 @@ async function markDayAsFull(ws, msg) {
                         getDaysWithBooking: getDaysWithBooking,
                         confirmBooking: confirmBooking,
                         unConfirmBooking: unConfirmBooking,
-                        removeBooking:removeBooking,
+                        removeBooking: removeBooking,
                         getAvailable: getAvailable,
                         addBooking: addBooking
                     }[msg["request"]];
